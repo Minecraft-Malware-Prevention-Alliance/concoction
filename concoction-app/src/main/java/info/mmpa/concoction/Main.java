873779fd26a23960e982251e01c6227aae4b4df4
Col-E <mcoley2@gmu.edu>
2023-07-08 05:19:14 -0400

    Clear out command line logic until API module logic is fleshed out

--- concoction-app/src/main/java/info/mmpa/concoction/Main.java
+++ concoction-app/src/main/java/info/mmpa/concoction/Main.java
@@ -1,35 +1,23 @@
 package info.mmpa.concoction;
 
-import java.io.File;
-import java.io.PrintWriter;
-import java.util.Map;
-import java.util.concurrent.Callable;
-
-import info.mmpa.concoction.result.ArchiveScanResults;
-import info.mmpa.concoction.result.printer.HumanReadablePrinter;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Parameters;
 
-@Command(name = "Concoction", mixinStandardHelpOptions = true, version = "Concoction v1.0.0", description = "Dynamic Shared Malware Scanner")
+import java.util.concurrent.Callable;
+
+@Command(name = "Concoction", mixinStandardHelpOptions = true,
+		version = "Concoction v" + ConcoctionBuildConfig.VERSION,
+		description = "Dynamic Shared Malware Scanner")
 public class Main implements Callable<Integer> {
-    @Parameters(index = "0", description = "The file / directory to scan")
-    private File file;
 
-    @Override
-    public Integer call() throws Exception {
-        Map<String, ArchiveScanResults> results = Concoction.scanDirectory(file);
+	@Override
+	public Integer call() throws Exception {
+        // TODO: Reimplement once the core library API solidifies
+		return 0;
+	}
 
-        if (results.isEmpty()) {
-            return CommandLine.ExitCode.OK;
-        } else {
-            new HumanReadablePrinter(results).print(new PrintWriter(System.out));
-            return 1;
-        }
-    }
-
-    public static void main(String[] args) {
-        final int exitCode = new CommandLine(new Main()).execute(args);
-        System.exit(exitCode);
-    }
+	public static void main(String[] args) {
+		final int exitCode = new CommandLine(new Main()).execute(args);
+		System.exit(exitCode);
+	}
 }
